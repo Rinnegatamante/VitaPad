c314cc2bead0b2c97618b0ff8cfb4a719d3515aa
Sayafdine Said <musikid@outlook.com>
2023-07-22 16:43:47 +0200

    fix: change heartbeat constant

--- server/src/heartbeat.hpp
+++ server/src/heartbeat.hpp
@@ -4,7 +4,7 @@
 #include <array>
 #include <cstdint>
 
-const std::array<uint8_t, 6> heartbeat_magic{0x45, 0x4e, 0x44,
-                                             0x48, 0x42, 0x54};
+const std::array<uint8_t, 6> heartbeat_magic{0xff, 0xff, 0xff,
+                                             0xff, 0x42, 0x54};
 
 #endif // HEARTBEAT_HPP
